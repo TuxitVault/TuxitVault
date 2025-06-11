d6a3142103f4affe2fbdd9e4f884b2ce1ebcab68
Florence EXTRAT <fextrat@evoliatis.com>
2025-06-11 19:28:56 +0200

    fix: welcome vue + login/register bg to match

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-cover bg-[url('/images/background.jpeg')]">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gradient-to-b from-green-800 to-white w-full">
         <div class="w-full sm:max-w-md mt-6 bg-white shadow-lg shadow-green-600 sm:rounded-lg">
             <div class="flex flex-col gap-2">
                 <div class="px-6 py-4 flex justify-center">
