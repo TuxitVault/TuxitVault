49cafb1c0fa5603895220606083523e449a89893
Lucky43620 <lulu.bruyere43@gmail.com>
2025-06-23 20:55:44 +0200

    Download + correction + ajout du local pour les tests

--- routes/web.php
+++ routes/web.php
@@ -42,5 +42,6 @@ Route::middleware([
         Route::post('/folder/create', 'createFolder')->name('folder.create');
         Route::post('/file', 'store')->name('file.store');
         Route::delete('/file', 'destroy')->name('file.delete');
+        Route::get('/file/download', 'download')->name('file.download');
     });
 });
